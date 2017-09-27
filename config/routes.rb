864b47f3c4da10377dc94dff5091cdbf36afa339
tsumi <kdy78022@gmail.com>
2017-09-27 18:28:13 +0900

    リソースベースのルーティングに変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
 
-  get '', to:'home#index',        as:'home'
+  root 'home#index'
 
-  get '', to:'send_words#index',  as:'send_words'
-  get '', to:'send_words#new',    as:'send_words_new'
-  get '', to:'send_words#create', as:'send_words_create'
-
+  # match '/send_words',        to:'send_words#index',  via:'get',  as:'send_words'
+  # match '/send_words/new',    to:'send_words#new',    via: ,  as:'send_words_new'
+  # match '/send_words/create', to:'send_words#create', via:'post', as:'send_words_create'
+  resources :send_words
 end
