11b18d2f36c9e8a45ec85e51dd42691286d77ca0
tsumi <kdy78022@gmail.com>
2017-12-12 20:30:14 +0900

    basic認証のコメント

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  # priduction環境の時はbasic認証をかける
   http_basic_authenticate_with name: ENV['BASIC_AUTH_USERNAME'], password: ENV['BASIC_AUTH_PASSWORD'] if Rails.env.production?
 end
