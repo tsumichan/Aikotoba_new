5c7787b5ef6076865158bc22c74c864825c61d85
tsumi <kdy78022@gmail.com>
2017-12-30 15:31:04 +0900

    エラーメッセージを日本語化

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,4 @@
 require_relative 'boot'
-
 require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
@@ -8,6 +7,7 @@ Bundler.require(*Rails.groups)
 
 module AikotobaNew
   class Application < Rails::Application
+    config.i18n.default_locale = :ja
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
