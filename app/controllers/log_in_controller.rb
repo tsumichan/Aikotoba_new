3b346f88d37112e7206d589d3ddce4bbed5150f6
tsumi <kdy78022@gmail.com>
2017-12-30 14:38:19 +0900

    ログイン処理を追加

--- app/controllers/log_in_controller.rb
+++ app/controllers/log_in_controller.rb
@@ -2,9 +2,29 @@ class LogInController < ApplicationController
   def index
   end
 
-  def create
+  def new
+    @log_in = User.new(log_in_params)
   end
 
-  def new
+  def create
+    @user = User.find_by(user_name: params[:user_name])
+
+    if @user.nil?
+      redirect_to users_new_path, warning: '新規登録をしてください'
+    elsif @user.user_name == params[:user_name] && @user.password == params[:password]
+      redirect_to root_path, success: 'ようこそ！'
+    else
+      flash.now[:danger] = 'ユーザー名かパスワードが間違っています'
+      render :new
+    end
+  end
+
+  private
+
+  def log_in_params
+    {
+      user_name: params[:user_name],
+      password:  params[:password]
+    }
   end
 end
