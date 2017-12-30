6606f453a4011e38434e986fdcaa180eea385da1
tsumi <kdy78022@gmail.com>
2017-12-30 14:37:57 +0900

    flashのタイプ追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  add_flash_types :success, :info, :warning, :danger
 
   # priduction環境の時はbasic認証をかける
   http_basic_authenticate_with name: ENV['BASIC_AUTH_USERNAME'], password: ENV['BASIC_AUTH_PASSWORD'] if Rails.env.production?
