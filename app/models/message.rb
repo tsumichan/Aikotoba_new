ea62e1eb0b579f3ee67b01022171a433075fe7be
tsumichan <kdy78022@gmail.com>
2017-12-31 11:43:11 +0900

    リレーション

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,6 @@
 class Message < ApplicationRecord
   validates :contents, presence: true
+
+  has_many :favorites, dependent: :destroy
+  has_many :user, through: :favorites
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171230162613) do
+ActiveRecord::Schema.define(version: 20171231015724) do
+
+  create_table "favorites", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer  "user_id",    null: false
+    t.integer  "message_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["message_id"], name: "index_favorites_on_message_id", using: :btree
+    t.index ["user_id"], name: "index_favorites_on_user_id", using: :btree
+  end
 
   create_table "messages", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "user_id"
